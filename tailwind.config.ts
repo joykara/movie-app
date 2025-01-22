e44aa908cf9077f5aec4eb5bb3342c8c1ccca851
joykara <joykarani1@gmail.com>
2025-01-22 23:16:56 +0300

    refactor: update installed dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,11 @@ import type { Config } from "tailwindcss";
 export default {
   darkMode: 'class',
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './common/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}'
   ],
   theme: {
     extend: {

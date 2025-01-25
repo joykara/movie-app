374e6517bea9c7b1f208fc259756d5b3a6659763
joykara <joykarani1@gmail.com>
2025-01-25 23:05:13 +0300

    fix: correct dynamic rendering for nextjs 15

--- app/(movierex)/movie/[id]/page.tsx
+++ app/(movierex)/movie/[id]/page.tsx
@@ -1,10 +1,8 @@
 import { fetchMovieById, fetchMovieCredits } from '@/services/actions';
 import MovieDetailsComponent from './movieDetails';
 
-export default async function MovieDetailsPage({
-    params
-}: Readonly<{params: { id: string }}>) {
-    const { id } = params;
+export default async function MovieDetailsPage({params}: {params: Promise<{ id: string }>}) {
+    const { id } = await params;
     const { movie_details, recommendations } = await fetchMovieById(id)
     const credits = await fetchMovieCredits(id);
 
